7e5b6e0d7dc6db678fd0ef2f45c88cadb9697b67
PaulaTole <pau.toledo@duocuc.cl>
2025-10-03 21:10:06 -0300

    casi

---
+++ src/test/java/com/inscripciones/autentificacion/AutentificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inscripciones.autentificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutentificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
